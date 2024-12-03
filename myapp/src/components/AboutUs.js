39d413ec19538d6f1e30ece5f40019dec42ca7a5
shanusen27 <139607388+shanusen27@users.noreply.github.com>
2024-12-03 11:27:42 +0000

    Link pages

---
+++ myapp/src/components/AboutUs.js
@@ -0,0 +1,7 @@
+export default function Contactus(){
+    return(
+        <div>
+            <h1>Welcome to Contact us page</h1>
+        </div>
+    );
+}
---
+++ myapp/src/components/Dashboard.js
@@ -0,0 +1,7 @@
+export default function Dashboard(){
+    return(
+        <div>
+            <h1>Hello Admin</h1>
+        </div>
+    );
+}
